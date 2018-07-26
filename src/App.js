434ee27d2d0548a3e20d34447e795dd4329357a0
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-07-26 15:17:03 +0300

    feat: Display map with first few default locations.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+//import { Map, InfoWindow, Marker } from 'google-maps-react';
 import './App.css';
+import GoogleApiWrapper from './components/MapContainer';
 
-import API from './components/API';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>Welcome to my neighborhood!</h1>
-				<API />
+				<div className="sidebar">
+
+				</div>
+				<div id="map">
+					<GoogleApiWrapper />
+				</div>
 			</div>
 		);
 	}
