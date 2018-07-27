b6fce73bf4eb90bfa48eb5c15f2a6e1bb1b2026e
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-07-27 11:34:40 +0300

    feat: Add Sidebar with search and add filtering maps
    Move styles to separate file

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,86 +2,7 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
 import { markers as defaultMarkers } from '../markers/markers';
 import Sidebar from './Sidebar';
-
-const styles = [
-    { height: '100%' },
-    {
-        featureType: 'water',
-        stylers: [
-            { color: '#19a0d8' }
-        ]
-    }, {
-        featureType: 'administrative',
-        elementType: 'labels.text.stroke',
-        stylers: [
-            { color: '#dddddd' },
-            { weight: 6 }
-        ]
-    }, {
-        featureType: 'administrative',
-        elementType: 'labels.text.fill',
-        stylers: [
-            { color: '#00495b' }
-        ]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [
-            { color: '#040063' },
-            { lightness: -40 }
-        ]
-    }, {
-        featureType: 'transit.station',
-        stylers: [
-            { weight: 9 },
-            { hue: '#f709e7' }
-        ]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'labels.icon',
-        stylers: [
-            { visibility: 'on' }
-        ]
-    }, {
-        featureType: 'water',
-        elementType: 'labels.text.stroke',
-        stylers: [
-            { lightness: 100 }
-        ]
-    }, {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [
-            { lightness: -100 }
-        ]
-    }, {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [
-            { visibility: 'on' },
-            { color: '#dbd9db' }
-        ]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'geometry.fill',
-        stylers: [
-            { color: '#efe9e4' },
-            { lightness: -25 }
-        ]
-    }, {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [
-            { color: '#02e234' }
-        ]
-    }, {
-        featureType: 'landscape.natural',
-        elementType: 'geometry',
-        stylers: [
-            { color: '#a4d89e' }
-        ]
-    }
-];
+import { styles } from './MapContainer.styles';
 
 export class MapContainer extends Component {
     state = {
@@ -92,7 +13,8 @@ export class MapContainer extends Component {
             lat: 56.345706,
             lng: 26.195000
         },
-        markers: [...defaultMarkers]
+        markers: [...defaultMarkers],
+        filteredmarkers: [...defaultMarkers]
     };
 
     onMarkerClick = (props, marker, e) =>{
@@ -102,11 +24,11 @@ export class MapContainer extends Component {
             showingInfoWindow: true
         });
         //When click on marker will have bounce animation
-        if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
-        } else {
-            marker.setAnimation(this.props.google.maps.Animation.BOUNCE);
-        }
+        // if (marker.getAnimation() !== null) {
+        //     marker.setAnimation(null);
+        // } else {
+        //     marker.setAnimation(this.props.google.maps.Animation.BOUNCE);
+        // }
     }
 
     onMapClicked = (props) => {
@@ -119,18 +41,21 @@ export class MapContainer extends Component {
     };
 
     /**
-     * Push new marker to our state
+     * set new markers to our state
+     * refresh the filter list with mainone
      * @param {Object} marker 
      */
-    pushMarker = (marker) => {
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                markers: [
-                    ...prevState.markers,
-                    marker
-                ]
-            };
+    updateMarkers = (markers) => {
+        this.setState({
+            markers: [...markers],
+            filteredmarkers: [...markers]
+        });
+    }
+
+    updateFilteredMarkers = (filteredmarkers) => {
+        //console.log("Updated filtered list", filteredmarkers);
+        this.setState({
+            filteredmarkers: [...filteredmarkers]
         });
     }
 
@@ -139,42 +64,49 @@ export class MapContainer extends Component {
         let bounds = new this.props.google.maps.LatLngBounds();
         let animation = this.props.google.maps.Animation.DROP;
        
-        const markers = this.state.markers;
+        const filteredmarkers = this.state.filteredmarkers;
 
-        markers.forEach((marker) =>{
+        filteredmarkers.forEach((marker) =>{
             bounds.extend(marker.position);
         });
 
         return (
             <React.Fragment>
                 <Sidebar
-                    handleChange={this.handleChange} pushMarker={this.pushMarker}/>
-                <Map
-                    styles={styles}
-                    google={this.props.google}
-                    initialCenter ={this.state.initialCenter}
-                    zoom={7}
-                    onClick={this.onMapClicked}
-                    bounds={bounds} >
-
-                { markers.map((mark, i) => {
-                        return <Marker onClick={this.onMarkerClick}
-                            key={i}
-                            name={mark.name}
-                            title={mark.title}
-                            position={mark.position}
-                            animation={animation} />
-                })}
-                
-                    <InfoWindow
-                        marker={this.state.activeMarker}
-                        visible={this.state.showingInfoWindow}>
-                        <div>
-                            <h1>{this.state.selectedPlace.name}</h1>
-                            <p>{this.state.selectedPlace.title}</p>
-                        </div>
-                    </InfoWindow>
-                </Map>
+                    markers={this.state.markers}
+                    filteredmarkers={this.state.filteredmarkers}
+                    updateMarkers={this.updateMarkers}
+                    updateFilteredMarkers={this.updateFilteredMarkers}
+                />
+                <div className="map">
+                    <Map
+                        styles={styles}
+                        google={this.props.google}
+                        initialCenter ={this.state.initialCenter}
+                        zoom={7}
+                        onClick={this.onMapClicked}
+                        bounds={bounds} >
+                    {
+                        filteredmarkers.map((mark, i) => {
+                            return <Marker onClick={this.onMarkerClick}
+                                key={i}
+                                name={mark.name}
+                                title={mark.title}
+                                position={mark.position}
+                                animation={animation} />
+                        })
+                    }
+                    
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}>
+                            <div>
+                                <h1>{this.state.selectedPlace.name}</h1>
+                                <p>{this.state.selectedPlace.title}</p>
+                            </div>
+                        </InfoWindow>
+                    </Map>
+                </div>
             </React.Fragment>
         );
     }
---
+++ src/components/MapContainer.styles.js
@@ -0,0 +1,79 @@
+export const styles = [
+    { height: '100%' },
+    {
+        featureType: 'water',
+        stylers: [
+            { color: '#19a0d8' }
+        ]
+    }, {
+        featureType: 'administrative',
+        elementType: 'labels.text.stroke',
+        stylers: [
+            { color: '#dddddd' },
+            { weight: 6 }
+        ]
+    }, {
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+            { color: '#00495b' }
+        ]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+            { color: '#040063' },
+            { lightness: -40 }
+        ]
+    }, {
+        featureType: 'transit.station',
+        stylers: [
+            { weight: 9 },
+            { hue: '#f709e7' }
+        ]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+            { visibility: 'on' }
+        ]
+    }, {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [
+            { lightness: 100 }
+        ]
+    }, {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+            { lightness: -100 }
+        ]
+    }, {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+            { visibility: 'on' },
+            { color: '#dbd9db' }
+        ]
+    }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [
+            { color: '#efe9e4' },
+            { lightness: -25 }
+        ]
+    }, {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [
+            { color: '#02e234' }
+        ]
+    }, {
+        featureType: 'landscape.natural',
+        elementType: 'geometry',
+        stylers: [
+            { color: '#a4d89e' }
+        ]
+    }
+];
