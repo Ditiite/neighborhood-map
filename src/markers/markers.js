9890ed94c2db6a2f7ec81a9d63493c60cbd223f8
Dita <dita.rahmane.silova@inbox.lv>
2018-07-28 10:46:30 +0300

    feat: Customize icon

--- src/markers/markers.js
+++ src/markers/markers.js
@@ -1,39 +1,44 @@
 export const markers = [
     {
         id: "1",
-        title: 'Līvānu pilsēta, LV-5316, Latvia',
-        name: 'Līvāni',
-        position: { lat: 56.355370, lng: 26.163225 },
-        type: 'museo'
+        title: 'Turaidas iela 10, Sigulda, Siguldas pilsēta, LV-2150, Latvia',
+        name: 'Turaidas pils',
+        position: { lat: 57.182273, lng: 24.850391 },
+        type: 'castle'
     },
     {
         id: "2",
-        title: 'Aglona Parish, LV-5304, Latvia',
-        name: 'Aglona',
-        position: { lat: 56.126584, lng: 27.015212 }
+        title: 'Gulbenes novads, Stāmerienas pagasts, Vecstāmeriena, LV-4406',
+        name: 'Stameriena castle',
+        position: { lat: 57.216338, lng: 26.899692 },
+        type: 'castle'
     },
     {
         id: "3",
         title: 'Daugavas iela 38, Daugavpils, LV-5401, Latvia',
         name: 'Daugavpils Fortress',
-        position: { lat: 55.882827, lng: 26.495144 }
+        position: { lat: 55.882827, lng: 26.495144 },
+        type: 'castle'
     },
     {
         id: "4",
         title: 'Pilsrundāle, Rundāles pagasts, LV-3921, Latvia',
         name: 'Rundāle Palace',
-        position: { lat: 56.413433, lng: 24.024491 }
+        position: { lat: 56.413433, lng: 24.024491 },
+        type: 'castle'
     },
     {
         id: "5",
         title: 'Vilces pagasts, LV-3026, Latvia',
         name: 'Blankenfelde',
-        position: { lat: 56.344261, lng: 23.547240 }
+        position: { lat: 56.344261, lng: 23.547240 },
+        type: 'castle'
     },
     {
-        id: "7",
+        id: "6",
         title: 'Cesvaines pilsēta, LV-4871, Latvia',
-        name: 'Cesvaine',
-        position: { lat: 56.969312, lng: 26.312596 }
+        name: 'Cesvaine palace',
+        position: { lat: 56.969312, lng: 26.312596 },
+        type: 'castle'
     }
 ];
