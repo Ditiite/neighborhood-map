872a0468a33ac4e55573fa28f8ae11a5b6221cc6
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-07-30 07:31:58 +0300

    feat: Add more location and weather API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-//import { Map, InfoWindow, Marker } from 'google-maps-react';
 import './App.css';
 import GoogleApiWrapper from './components/MapContainer';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			marker: []
-		}
-	}
 
 	render() {
 		return (
