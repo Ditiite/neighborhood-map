c8ada3e04688c5de643f836be912d4f745940e76
Dita <28303111+Ditiite@users.noreply.github.com>
2018-07-31 13:40:51 +0300

    fix: error with weather API

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -6,11 +6,11 @@
  * @retuns {WeatherInfo}
  */
 export async function getWeather(lat, lng) {
-    const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=4081a6920c100a1824eff93069bb26e0`);
+    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=4081a6920c100a1824eff93069bb26e0`);
     const data = await resp.json();
     return {
         description: data.weather[0].description,
-        weatherIcon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+        weatherIcon: `https://openweathermap.org/img/w/${data.weather[0].icon}.png`,
         temp: (data.main.temp - 273.15).toFixed(1)
     };
 }
