61106d64caf6ceb3740180e5b1dbeface83cfb08
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-09-12 08:10:57 +0300

    Refactoring: make new file for ApiWrapper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import GoogleApiWrapper from './components/MapContainer';
+import GoogleApiWrapper from './components/ApiWrapper';
 
 class App extends Component {
 
